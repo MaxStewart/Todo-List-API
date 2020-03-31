4f1316f271295cfe953aeaf96420758cfb2690fb
stewartmax <stewartmax100@gmail.com>
2020-03-31 21:34:31 +1300

    created a user and todolist classes

---
+++ TodoApi/TodoApi/Models/UsersTodoList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class UsersTodoList
+    {
+        public UserAccount UserAccount { get; set; }
+        public List<TodoItem> TodoList { get; set; }
+    }
+}
