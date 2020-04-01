e35fa4e946703da80718cf824af2dccaebe65244
stewartmax <stewartmax100@gmail.com>
2020-04-01 21:30:40 +1300

    Added Swagger UI for explaining my API

--- TodoApi/TodoApi/Startup.cs
+++ TodoApi/TodoApi/Startup.cs
@@ -12,6 +12,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using TodoApi.Models;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Reflection;
+using System.IO;
 
 namespace TodoApi
 {
@@ -29,6 +33,34 @@ namespace TodoApi
         {
             services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
+
+            // Register Swagger generator
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "My API",
+                    Version = "v1",
+                    Description = "",
+                    TermsOfService = new Uri("https://max-stewart.com/"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "",
+                        Email = string.Empty,
+                        Url = new Uri("https://max-stewart.com/"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = string.Empty,
+                        Url = new Uri("https://max-stewart.com/"),
+                    }
+                });
+
+                // Set comments path for Swagger JSON and UI
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +70,14 @@ namespace TodoApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui and specify endpoint
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
